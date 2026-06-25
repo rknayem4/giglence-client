b592182d7c254e79d248e70865c4bd6b0062999d
rknayem4 <rknayem10@gmail.com>
2026-06-25 11:46:00 +0600

    added role req

--- src/app/(dashboard)/dashboard/client/layout.jsx
+++ src/app/(dashboard)/dashboard/client/layout.jsx
@@ -1,4 +1,5 @@
 import SidebarClient from "@/Components/ClientCom/SidebarClient";
+import { RequireRole } from "@/lib/session";
 
 export default async function ClientLayout({ children }) {
   await RequireRole("client");
